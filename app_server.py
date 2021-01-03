fef1788565a6f0dbed832ccbf251655aeff058f6
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-01-03 20:29:46 -0300

    initial commit for app_server.py at homework 01: only server.hello() remote call object method is working. Increase chaining objects in future

---
+++ app_server.py
@@ -0,0 +1,194 @@
+from modules.profile import Profile
+import Pyro4
+from typing import List, Dict
+
+
+"""
+in terminal 01:
+python -m Pyro4.naming
+
+in terminal 02:
+python app_server.py
+"""
+
+
+db_li_profiles: List[Profile] = []
+p1 = Profile(first_name='Bruno', last_name='Conde Costa da Silva', photo='bruno.png', address='Ananindeua', email='bruno@email.com', academic_education='Engenharia da Computação', skills=['Python'], experiences=['Ciencia de dados', 'Desenvolvimento Web'])
+# p1.photo.show()
+p2 = Profile(first_name='Renato', last_name='Sousa da Silva', photo='renato.png', address='Ananindeua', email='renato@email.com', academic_education='Engenharia da Computação', skills=['Cobol'], experiences=['infra'])
+p3 = Profile(first_name='Cassio', last_name='Sousa da Silva', photo='cassio.png', address='Bragança', email='cassio@email.com', academic_education='Educação Física', skills=['Cobol'], experiences=['xadrez na praça'])
+
+db_li_profiles.append(p1)
+db_li_profiles.append(p2)
+db_li_profiles.append(p3)
+
+# for profile in db_li_profiles:
+	# print('\n--------------------')
+	# print(profile.__dict__)	
+
+@Pyro4.expose # wihtout this line the client doesn't run properly
+class Server:
+	"""
+	Class Server to create exposed server object
+	"""	
+	# db_li_profiles: List[Profile] = db_li_profiles
+
+
+	def __init__(self): pass		
+	
+	@staticmethod	
+	def hello(msg: str) -> str:		
+		print('client message: {}'.format(msg))
+		return 'Message processed sucessfully by the server application'
+
+	# 01
+	@staticmethod
+	def list_profiles_of_a_course(course: str) -> list:
+		"""
+		List all profiles of a course
+		"""
+
+		db_li_profiles_of_a_course = []
+		for profile in db_li_profiles:			
+			if profile.academic_education == course:
+				db_li_profiles_of_a_course.append(profile)
+
+		return db_li_profiles_of_a_course
+
+	# 02
+	@staticmethod
+	def list_skills_of_profiles_of_a_city(address: str) -> list:
+		"""
+		List the skills of the profiles of a specific city
+		"""
+
+		li_skills_of_profiles_of_a_address = []
+		for profile in db_li_profiles:
+			if profile.address == address:
+				if type(profile.skills) == list:
+					for skill in profile.skills:
+						if not skill in li_skills_of_profiles_of_a_address:
+							li_skills_of_profiles_of_a_address.append(skill)
+				else:
+					return [profile.skills]
+
+		return li_skills_of_profiles_of_a_address
+
+	# 03
+	@staticmethod
+	def put_new_experience_in_a_profile(profile: Profile, experience: str) -> None:
+		"""
+		Addding a new experience in a registrated profile
+		:profile 	-> Profile
+		:experience -> str 
+		"""
+		profile.experiences.append(experience)
+
+	# 04
+	@staticmethod
+	def list_experiences_from_email_profile(email: str):
+		"""
+		Get experiences from an email profile of registered profiles		
+		"""
+		li_email = [profile.email for profile in db_li_profiles]			
+
+		if len(li_email) == 0:
+			print(f"{email} email does'n belong to any users")
+			return None
+
+		if email in li_email:
+			for profile in db_li_profiles:
+				if profile.email == email:
+					return profile.experiences
+
+	# 05
+	def list_all_informations_of_all_profiles(self) -> Dict:
+		"""Get all informations of all profiles
+		:db_li_profiles: List[Profiles]
+		return: Dict -> Dictionary of all profiles
+		"""
+		di_all_profiles = {}
+
+		for profile in db_li_profiles:
+			email = profile.email
+			di_all_profiles[email] = {}
+
+			for key, value in profile.__dict__.items():
+				if not key == 'email':
+					di_all_profiles[email][key] = value			
+
+		return di_all_profiles
+
+	# 06
+	def list_all_informations_of_profile_by_its_email(self, email: str) -> Dict:
+		"""Get all informations of a profile by passing its email
+		:db_li_profiles: List(Profile)
+		:email: str -> The email profile
+		return: Dict -> Return the profile informations in a dictionary
+		"""
+		try:
+			return self.list_all_informations_of_all_profiles()[email]
+		except Exception as e:
+			print('Email {} not found:\n\n {}'.format(email, e))
+			raise e
+
+
+def startServer():
+    server = Server()
+
+    # make a Pyro daemon to listen remote calls
+    # all pyro objects are registered in one or more deamons
+    daemon = Pyro4.Daemon() 
+
+    # locate the name server running
+    # usually there is just one name server running in your network
+    ns = Pyro4.locateNS()
+
+    # register the server as a Pyro object
+    # uri is unique resource identifier (the same idea of url - unique resource location)
+    uri = daemon.register(server)  
+    
+    # register the object with a name in the name server
+    ns.register("server", uri)   
+    
+    # print the uri so we can use it in the client later
+    print("Ready. Object uri =", uri)
+    
+    # start the event loop of the server to wait for calls
+    daemon.requestLoop()    
+
+
+if __name__ == '__main__':
+	startServer()
+
+# -------------------------------------------------
+
+# testing (client code)
+# server = Server() # don't dothis in client
+
+# # 01
+# profiles_engcomp = server.list_profiles_of_a_course(course='Engenharia da Computação')
+# for profile in profiles_engcomp:
+# 	print(profile.__dict__)
+
+# # 02
+# skills_ananindeua = server.list_skills_of_profiles_of_a_city(address="Ananindeua")
+# print(skills_ananindeua)
+
+# # 03
+# print('Bruno experiences: {}'.format(db_li_profiles[0].experiences)) # will not work on client
+# db_li_profiles[0].experiences.append('Engenharia de software')
+# print('Bruno experiences: {}'.format(db_li_profiles[0].experiences)) # will not work on client
+
+# # 04
+# print('Getting experiences from profile which owns to {}'.format("cassio@email.com"))
+# print(server.list_experiences_from_email_profile(email='cassio@email.com'))
+
+# # 05
+# di_all_informations = server.list_all_informations_of_all_profiles()
+# print(di_all_informations)
+
+# # 06
+# email_informations = server.list_all_informations_of_profile_by_its_email(email="renato@email.com")
+# print(email_informations)
+# email_informations['photo'].show()
