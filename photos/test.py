155b8e45762a80a0fe8c218ce9451b48148134b5
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-01-04 06:06:52 -0300

    (test) encode and decode an image in photos/test.py

---
+++ photos/test.py
@@ -0,0 +1,40 @@
+from PIL import Image
+import base64
+import io
+
+
+###########################
+# Serializing/encoding (Server)
+
+image = open('bruno.png', 'rb')
+print(image)
+
+# # optional at the server side and usefull in client side
+# # It does not work at the same time that client
+# with Image.open(image) as image_pillow_server:
+# 	print(type(image_pillow_server))
+# 	image_pillow_server.show()
+
+print('\n\n' + 30 * '-')
+image_read = image.read()
+print(image_read)
+
+print('\n\n' + 30 * '-')
+image_64_encode = base64.encodebytes(image_read)
+print(image_64_encode)
+
+###############################
+# Deserializing/Decoding (Client)
+
+image_64_decode = base64.decodebytes(image_64_encode)
+print(type(image_64_decode))
+
+# optional at the server side and usefull in client side
+# image_pillow_client = Image.open(io.BytesIO(image_64_decode))
+# print(type(image_pillow_client))
+# image_pillow_client.show()
+
+# # or
+
+with Image.open(io.BytesIO(image_64_decode)) as image_pillow_client:
+	image_pillow_client.show()
