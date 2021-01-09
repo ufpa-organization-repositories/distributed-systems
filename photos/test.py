cb972f21ae2e58ea1c88a262735c53de8242cb5d
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-01-09 01:48:52 -0300

    (test.py, at photos/) [print]-> Just some prints of types of image variable in each coding/decoding phase

--- photos/test.py
+++ photos/test.py
@@ -8,6 +8,7 @@ import io
 
 image = open('bruno.png', 'rb')
 print(image)
+print('image type: ', type(image))
 
 # # optional at the server side and usefull in client side
 # # It does not work at the same time that client
@@ -17,11 +18,13 @@ print(image)
 
 print('\n\n' + 30 * '-')
 image_read = image.read()
-print(image_read)
+print('image_read: ', image_read)
+print('image_read (type): ', type(image_read))
 
 print('\n\n' + 30 * '-')
 image_64_encode = base64.encodebytes(image_read)
-print(image_64_encode)
+print('image_64_encode: ', image_64_encode)
+print('image_64_encode (type): ', type(image_64_encode))
 
 ###############################
 # Deserializing/Decoding (Client)
@@ -30,11 +33,11 @@ image_64_decode = base64.decodebytes(image_64_encode)
 print(type(image_64_decode))
 
 # optional at the server side and usefull in client side
-# image_pillow_client = Image.open(io.BytesIO(image_64_decode))
-# print(type(image_pillow_client))
-# image_pillow_client.show()
+image_pillow_client = Image.open(io.BytesIO(image_64_decode))
+print(type(image_pillow_client))
+image_pillow_client.show()
 
 # # or
 
-with Image.open(io.BytesIO(image_64_decode)) as image_pillow_client:
-	image_pillow_client.show()
+# with Image.open(io.BytesIO(image_64_decode)) as image_pillow_client:
+# 	image_pillow_client.show()
