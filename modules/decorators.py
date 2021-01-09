3e2074a287c01c1f0dad8e4b653cd307e191f6d6
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-01-09 19:06:17 -0300

    (decorators.py, at modules/) -> plot the best and worst time between the executions of an operation

--- modules/decorators.py
+++ modules/decorators.py
@@ -50,7 +50,7 @@ def calc_time(debug: bool = True) -> float:
 	# return decorator(function)
 	return decorator
 
-def calc_plot(function: Callable, debug: bool = True):	
+def calc_plot(function: Callable, debug: bool = False):	
 	"""
 	Decoration function: plots the graph of a time
 	that a function takes to execute
@@ -74,34 +74,48 @@ def calc_plot(function: Callable, debug: bool = True):
 		or other anything return type defined in this wrapper
 		(in this case, None, beacuse only displays the graph) 
 		"""
-		n_plots: int = 100
+		n_plots: int = 20
 		li_exec_time: list = []		
 		for i in range(n_plots):
 			result: float = function(*args, **kwargs) # returns the execution time
 			li_exec_time.append(result)
 
-		plt.figure(figsize=(10, 6))
+		plt.figure(figsize=(12, 6))
 		plt.plot(range(1, n_plots + 1), li_exec_time)
 		plt.axis(xmin=0, xmax=n_plots + 1)
 		plt.title('time per execution')
 		plt.ylabel('time (s)')
 		plt.xlabel('executions')
 
-		plt.scatter(range(1, n_plots + 1), li_exec_time, marker='.', label="Execution", color="black")
+		plt.scatter(range(1, n_plots + 1), li_exec_time, marker='.',\
+		 label="Execution", color="black")
+
+		plt.scatter(1 + li_exec_time.index(min(li_exec_time)),\
+		 min(li_exec_time), marker='s', label=f"Best time = {min(li_exec_time)} s",\
+		  color="green")
+
+		plt.scatter(1 + li_exec_time.index(max(li_exec_time)),\
+		 max(li_exec_time), marker='*', label=f"Worst time = {max(li_exec_time)} s",\
+		  color="red")
+
 		plt.legend(bbox_to_anchor=(1, 1), loc='best', fancybox=True, framealpha=1)
 
 		if debug:		
 			print('ploting')
 			plt.show()
 		else:		
-			plt.savefig('fig.png')
+			plt.savefig('time_operation.png')
 		
 		return plt
 
 	return wrapper
+
+
 # -------------------------------------
 # testing
 
+# """
+# @calc_plot(debug=True) # TypeError: calc_plot() missing 1 required positional argument: 'function'
 @calc_plot # test with and without this
 @calc_time(debug=True) # test with and without this
 def operation(sleep_time: int = 1) -> str:	
@@ -112,4 +126,4 @@ if __name__ == '__main__':
 	result = operation(sleep_time=0.05)
 	# print(result)
 	# result.show()
-	
+# """
