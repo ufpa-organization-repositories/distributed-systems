cc8a4ac97bafca0809dc78eb534ac02e177e6d7e
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-01-09 14:16:27 -0300

    (decorators.py, at modules/) -> nested decorators minimally working, @calc_plot@pcalc_time

--- modules/decorators.py
+++ modules/decorators.py
@@ -1,5 +1,6 @@
 from typing import Callable, Any
 from time import time, sleep
+# from matplotlib import pyplot
 
 
 def calc_time(debug: bool = True) -> float:
@@ -13,7 +14,7 @@ def calc_time(debug: bool = True) -> float:
 		Decoration function: calculates the tima a function
 		takes to execute
 
-		Will used to check the time took from each operation,
+		Will be used to check the time took from each operation,
 		from 01 to 06 ones
 		"""
 
@@ -21,28 +22,62 @@ def calc_time(debug: bool = True) -> float:
 			"""
 			wraps/envolves the operation function,
 			calculating the time it takes to execute
+			return: Any -> Because can return both the result of the initial
+			function or other anything return type defined in this wrapper
+			(in this case, the time of execution of the function) 
 			"""
 
 			start_time = time()	
-			result = function(*args, **kwargs)
+			result = function(*args, **kwargs) # the operation itself
 			end_time = time()
 
 			exec_time = end_time - start_time
 			if debug == True:
 				print(f'time that the operation takes to execute: \n \
 					>>>{exec_time}')
-			return result
+
+			# return result # the original return in function/operation
+			return exec_time # to use without @calc_plot
+			# return function()
+
 
 		return wrapper
 
+	# return decorator(function)
 	return decorator
 
+def calc_plot(function: Callable):	
+	"""
+	Decorator Factory Design Pattern to 
+	plot a time an operation takes to
+	execute
+	"""
 
+	# def decorator(function: Callable) -> Callable:		
+	# 	"""
+	# 	Decoration function: plots the graph of a time
+	# 	that a function takes to execute
+
+	# 	Will be used to plot the time took from each operation,
+	# 	from 01 to 06 ones
+	# 	"""
+	def wrapper(*args, **kwargs):
+		result = function()
+		return 'ploted'
+		pass
+		# return exec_time
+		# function(args, kwargs)
+		# if debug == True:
+		# 	print('exec_time: ')
+		# 	print(f'graph of exec_time')
+
+	return wrapper
 # -------------------------------------
 # testing
 
-@calc_time(debug=False) # test with and without this
-def operation() -> None:
+@calc_plot # test with and without this
+@calc_time(debug=True) # test with and without this
+def operation() -> str:
 	sleep_time = 3
 	sleep(sleep_time)
 	return f'sleep for {sleep_time} seconds'
